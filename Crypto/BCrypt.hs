008fd392119799a5c8957e5c996dbe8be956ce51
Andrew Miller <andrew@amxl.com>
2016-05-01 13:23:49 +1000

    Fix syntax following pull request #10, bump version

--- Crypto/BCrypt.hs
+++ Crypto/BCrypt.hs
@@ -18,7 +18,7 @@
  -}
 module Crypto.BCrypt (HashingPolicy(..), hashPasswordUsingPolicy, validatePassword,
                       fastBcryptHashingPolicy, slowerBcryptHashingPolicy,
-                      hashUsesPolicy, hashPassword, genSalt, genSaltUsingPolicy
+                      hashUsesPolicy, hashPassword, genSalt, genSaltUsingPolicy,
                       defaultHashAlgorithm)
 where
 
